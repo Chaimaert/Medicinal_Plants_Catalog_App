b9294c6c600985995a93fef95cd5e8d8366b476e
chaimaert <chaima.Rouita@emsi-edu.ma>
2024-11-28 23:45:59 +0100

    fetching data

--- Frontend/src/app/services/plant.service.ts
+++ Frontend/src/app/services/plant.service.ts
@@ -1,4 +1,3 @@
-// src/app/services/plant.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -8,17 +7,12 @@ import { Plant } from '../models/plant.model';
   providedIn: 'root'
 })
 export class PlantService {
-  private apiUrl = 'http://localhost:8084/api/plants'; // Adjust the URL to your backend
+  private apiUrl = 'http://localhost:8080/plantes'; //  backend URL
 
   constructor(private http: HttpClient) {}
 
-  // Get all plants from the backend
+  // Fetch all plants
   getAllPlants(): Observable<Plant[]> {
     return this.http.get<Plant[]>(this.apiUrl);
-  }
-
-  // Get plants by name, filter by first letter
-  getPlantsByLetter(letter: string): Observable<Plant[]> {
-    return this.http.get<Plant[]>(`${this.apiUrl}?letter=${letter}`);
   }
 }
